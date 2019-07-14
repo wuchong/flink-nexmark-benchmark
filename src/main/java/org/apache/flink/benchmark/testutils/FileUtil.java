7bb076f8606c35ed173bd1d9612f8689a05eda9a
Fan Xie <fx349@nyu.edu>
2019-07-14 15:13:06 -0700

    query 0 & 2

---
+++ src/main/java/org/apache/flink/benchmark/testutils/FileUtil.java
@@ -0,0 +1,20 @@
+package org.apache.flink.benchmark.testutils;
+
+import java.io.File;
+
+public class FileUtil {
+
+    public static void deleteFolder(File folder) {
+        File[] files = folder.listFiles();
+        if(files!=null) { //some JVMs return null for empty dirs
+            for(File f: files) {
+                if(f.isDirectory()) {
+                    deleteFolder(f);
+                } else {
+                    f.delete();
+                }
+            }
+        }
+        folder.delete();
+    }
+}
